5818dc231bcde75ce83d079c6a16a3344b579a11
xiejie <445383232@qq.com>
2025-09-24 09:37:12 +0800

    取消日志

--- caiji.py
+++ caiji.py
@@ -8,16 +8,16 @@ import logging
 from datetime import datetime
 # nihao
 
-# 配置日志
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler("sensor.log"),
-        logging.StreamHandler()
-    ]
-)
-logger = logging.getLogger("TempHumiditySensor")
+# # 配置日志
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#     handlers=[
+#         logging.FileHandler("sensor.log"),
+#         logging.StreamHandler()
+#     ]
+# )
+# logger = logging.getLogger("TempHumiditySensor")
 
 
 class TemperatureHumiditySensor:
